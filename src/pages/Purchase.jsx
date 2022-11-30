86bafb37c93837ebdab4aa78022a73d675d48b44
BrianMacia <brianmacia@hotmail.com>
2022-11-30 02:17:19 -0300

    v5

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -15,7 +15,7 @@ const Purchase = () => {
 
 
     return (
-        <div>
+        <div className='purchasestyle'>
             <h1>Purchase</h1>
             <ul>
                 {
@@ -23,10 +23,11 @@ const Purchase = () => {
                         <li key={purchase.id}>
                             {purchase.cart.products.map(product => (
                                 <li>
+                                    <h2><b>Purchase Date :</b>{product.createdAt}</h2>
                                     <h3><b>Product ID :</b>{product.id}</h3>
                                     <h3><b>Product : </b>{product.title}</h3>
                                     <h3><b>Price :</b>{product.price}</h3>
-                                    <h3><b>Purchase Date :</b>{product.createdAt}</h3>
+                                    
                                  </li>
                            ))}
                         </li>
