caeb2da64205c75fed85f2100b70adba06df8a3b
BrianMacia <brianmacia@hotmail.com>
2022-11-30 01:33:38 -0300

    v3

--- src/store/slices/purchase.slice.jsx
+++ src/store/slices/purchase.slice.jsx
@@ -1,21 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { setIsLoading } from './isLoading.slice';
+import getConfig from '../../utils/getConfig';
 
 export const PurchaseSlice = createSlice({
     name: 'purchase',
     initialState: [],
     reducers: {
         setPurchase: (state, action) => {
-                return action.payload;
+            return action.payload;
         }
     }
 })
 
 export const getPurchaseThunk = () => (dispatch) => {
     dispatch(setIsLoading(true));
-    return axios.get(`https://e-commerce-api.academlo.tech/api/v1/purchases`)
-        .then(() => dispatch(setPurchase(res.data)))
+    return axios.get(`https://e-commerce-api.academlo.tech/api/v1/purchases`, getConfig())
+        .then((res) => dispatch(setPurchase(res.data.data.purchases)))
         .finally(() => dispatch(setIsLoading(false)));
 }
 
